781269a8ba1cce41ff3fc46c01614893016bf99a
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-11-05 10:00:01 +0530

    create and register file upload page

--- Anubis/urls.py
+++ Anubis/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from anubis.views import homePage
+from anubis.views import homePage, fileUpload
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homePage, name='homePage')
+    path('', homePage, name='homePage'),
+    path('file-upload', fileUpload, name='fileUpload')
 ]
--- anubis/views.py
+++ anubis/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def homePage(request):
     return render(request, "index.html")
+
+def fileUpload(request):
+    return render(request, "fileupload.html")
